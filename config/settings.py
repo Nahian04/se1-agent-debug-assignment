9850710133c14d40bbd80b67102745d4f065e363
Nahian04 <nahianibnasad@gmail.com>
2025-08-24 21:39:23 +0600

    feat: add settings

---
+++ config/settings.py
@@ -0,0 +1,9 @@
+import os
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+# Path to KB JSON file
+KB_FILE_PATH = os.path.join(BASE_DIR, "data", "kb.json")
+
+# Path to logger directory
+LOG_DIR = "logs"
